78db3da66552410ca9cd2043ec0aae4f76bad223
angela-yen <angela.yen0210@gmail.com>
2022-03-27 21:15:26 +1300

    created task table

--- server/db/migrations/20220324203029_tasks.js
+++ server/db/migrations/20220324203029_tasks.js
@@ -1,15 +1,11 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
-  
-};
+exports.up = function (knex) {
+  return knex.schema.createTable('Tasks', (table) => {
+    table.increments().primary()
+    table.string('task')
+    table.string('catagory')
+  })
+}
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
-};
+exports.down = function (knex) {
+  return knex.schema.dropTable('Tasks')
+}
