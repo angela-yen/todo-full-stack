022ae0695af4757be78c4b4a817ca0cbb5baf36b
angela-yen <angela.yen0210@gmail.com>
2022-03-31 18:25:23 +1300

    created apis

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -1,6 +1,47 @@
 const express = require('express')
+const db = require('../db/tasks')
 
 const router = express.Router()
 module.exports = router
 
-router.get()
+// CRUD
+
+// POST route for /api/v1/tasks
+router.post('/', (req, res) => {
+  db.addTask(req.body)
+    .then(() => {
+      res.sendStatus(201)
+      return null
+    })
+    .catch((err) => {
+      console.error(err)
+      res.status(500).json({ message: 'error in server' })
+    })
+})
+
+// GET route for /api/v1/tasks
+router.get('/', (req, res) => {
+  db.getAllTasks()
+    .then((tasks) => {
+      res.json(tasks)
+      return null
+    })
+    .catch((err) => {
+      res.status(500).send('DATABASE ERROR: ' + err.message)
+    })
+})
+
+// PATCH route for /api/v1/tasks
+router.patch('/', (req, res) => {
+  const { id, updatedTask } = req.body
+  db.updateTask(id, updatedTask)
+    .then((tasks) => {
+      res.json(tasks)
+      return null
+    })
+    .catch((err) => {
+      res.status(500).send('DATABASE ERROR: ' + err.message)
+    })
+})
+
+// DELETE route for /api/v1/Tasks
